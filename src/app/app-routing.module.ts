bbe7cb32c7b078de12c3a209d378630b6011d18e
diegoeherrera <deherrera@gmail.com>
2020-01-12 13:38:50 -0300

    update branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
- import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ModalComponent } from './users/modal/modal.component';
 
-
-/* const routes: Routes = [
-  {path:'edit/:id', component:ModalComponent}
+const routes: Routes = [
+  {path:'edit/:id', component: ModalComponent}
 ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-}) */
+})
+
 export class AppRoutingModule { }
 
